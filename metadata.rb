15084b349b4599e87f4d9c0ec168395a5d432843
Chris Horton <hortoncd@gmail.com>
2016-11-29 09:27:58 -0800

    fix FC064 and FC065 (urls)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ license          'Apache 2.0'
 name             'alternate_ruby'
 description      'Installs/Configures assorted non-standard ruby packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/hortoncd/cookbook-alternate-ruby'
+issues_url       'https://github.com/hortoncd/cookbook-alternate-ruby/issues'
 version          '0.1.1'
 
 supports 'ubuntu'
