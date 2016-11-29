a962d109e4f52d4a198a6a319d91a910e5a429a2
Chris Horton <hortoncd@gmail.com>
2016-11-29 09:23:45 -0800

    remove minitest in favor of serverspec

--- test/cookbooks/alternate_ruby_test/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'alternate_ruby_test'
-maintainer       'Chris Horton'
-maintainer_email 'hortoncd@gmail.com'
-license          'Apache 2.0'
-description      'Installs/Configures alternate_ruby_test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends "alternate_ruby"
